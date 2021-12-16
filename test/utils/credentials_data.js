89eb9bac0e43729bcaf11acd079c9fca2a97b0d4
Jane <43620921+janekimpak@users.noreply.github.com>
2021-12-15 23:17:55 -0500

    added new signin (#1)

---
+++ test/utils/credentials_data.js
@@ -0,0 +1,10 @@
+// export function generateUserEmail(length, characters) {
+//     let first = "test.";
+//     for (let i = 0; i < length; i++) {
+//         first += characters.charAt(
+//             Math.floor(Math.random() * characters.length)
+//         );
+//     }
+//     return first + "@gmail.com";
+// }
+// ;
